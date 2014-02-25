eed8b94ebe3c1fd7d963ba9f12439b108d52d76d
Ricky Elrod <ricky@elrod.me>
2014-02-25 16:59:54 -0500

    Sponsorship info, fix index, fix site.hs indentation

--- site.hs
+++ site.hs
@@ -1,21 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Hakyll
+import Hakyll
 
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+      route idRoute
+      compile compressCssCompiler
 
     match "templates/*" $ compile templateCompiler
 
     match "*.html" $ do
-        route $ setExtension "html"
-        compile $ do
-            getResourceBody
-                >>= loadAndApplyTemplate "templates/default.html" defaultContext
-                >>= relativizeUrls
+      route $ setExtension "html"
+      compile $ do
+        getResourceBody
+          >>= loadAndApplyTemplate "templates/default.html" defaultContext
+          >>= relativizeUrls
