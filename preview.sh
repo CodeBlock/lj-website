0caa383540d645e6b6f21d51227dfc1bc29499dd
Ricky Elrod <ricky@elrod.me>
2014-05-13 11:32:44 -0400

    Add registration page
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- preview.sh
+++ preview.sh
@@ -2,6 +2,8 @@
 
 cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
 
+rm -rf "$cwd/_site/" "$cwd/_cache/"
+
 if [ ! -f "$cwd/dist/build/lambdajam/lambdajam" ]; then
   echo "lambdajam binary not found."
   exit 1
