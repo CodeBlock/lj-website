160a58932b2a71062ae40012439827ae3966ea74
Ricky Elrod <ricky@elrod.me>
2014-05-28 13:17:40 -0400

    publish the schedule in a very annoying way

--- site.hs
+++ site.hs
@@ -22,6 +22,10 @@ main = do
 
     match "templates/*" $ compile templateCompiler
 
+    match "content.html" $ do
+      route idRoute
+      compile copyFileCompiler
+
     match "*.html" $ do
       let ctx = mconcat [ constField "ts" ts'
                         , defaultContext
